fb745bcbef7c824075c63f21fcf88557cb2bfde3
YukiGreen <zxz938@outlook.com>
2021-01-26 23:54:32 +0800

    zxz nodejs内容提交

---
+++ index.js
@@ -0,0 +1,12 @@
+// const os = require("os");
+// console.log(os.tmpdir());
+// console.log(os.cpus().length);
+const fs = require('fs');
+const path = require('path');
+async function test() {
+const fromFilename = path.resolve(__dirname, "./myfiles/1.jpeg");
+const buffer = await fs.readFile(fromFilename);
+const toFilename = path.resolve(__dirname, "./myfiles/1.copy.jpeg");
+await fs.promises.writeFile(toFilename, buffer);
+console.log('copy success！');
+}
